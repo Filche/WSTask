8421fb304d28c59e5d4bdc48fb0c7ad63e8ed1ca
Filche <102279429+Filche@users.noreply.github.com>
2022-07-14 20:07:46 +1000

    Add files via upload

---
+++ PostCards/src/company/Post.java
@@ -0,0 +1,17 @@
+package company;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Post {
+    private UUID id;
+    private String name;
+}
